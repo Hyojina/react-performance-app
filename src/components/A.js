109d9b2906ee2a63df35aa58661d94a2c0a5ab9d
Hyojina <hjkim3009@gmail.com>
2022-12-29 21:02:47 +0900

    feat: Profiler 성능 측정 앱 제작

---
+++ src/components/A.js
@@ -0,0 +1,24 @@
+// A: 모든 요소를 하나의 컴포넌트에 넣기
+// 스니펫: rafce
+
+import React from "react";
+
+const A = ({ message, posts }) => {
+  return (
+    <div>
+      <h1>A Component</h1>
+      <p>{message}</p>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              <p>{post.title}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default A;
---
+++ src/components/B.js
@@ -0,0 +1,37 @@
+// B: 여러 컴포넌트로 나눠주기
+
+import React from "react";
+
+const Message = ({ message }) => {
+  return <p>{message}</p>;
+};
+
+const ListItem = ({ post }) => {
+  return (
+    <li>
+      <p>{post.title}</p>
+    </li>
+  );
+};
+
+const List = ({ posts }) => {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <ListItem key={post.id} post={post} />
+      ))}
+    </ul>
+  );
+};
+
+const B = ({ message, posts }) => {
+  return (
+    <div>
+      <h1>B Component</h1>
+      <Message message={message} />
+      <List posts={posts} />
+    </div>
+  );
+};
+
+export default B;
